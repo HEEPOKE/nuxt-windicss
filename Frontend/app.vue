6a2a0a0ff3835a9e02ff0cf23bd6de634eb525d5
HEEPOKE <77528292+HEEPOKE@users.noreply.github.com>
2022-10-31 23:14:57 +0700

    set up again

--- Frontend/app.vue
+++ Frontend/app.vue
@@ -1,10 +1,5 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="bg-gray-500 w-1/3 rounded-lg mt-6">
+    aaa
   </div>
 </template>
-
-<script>
-import 'virtual:windi.css';
-
-</script>
--- Frontend/nuxt.config.ts
+++ Frontend/nuxt.config.ts
@@ -1,18 +1,6 @@
-// import { defineNuxtConfig } from 'nuxt3';
-
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    buildModules: [
+    modules: [
         'nuxt-windicss',
       ],
-      windicss: {
-        scan: {
-          dirs: ['./'],
-          exclude: [
-            'node_modules',
-            'dist',
-            '.nuxt',
-          ],
-        },
-      },
 })
