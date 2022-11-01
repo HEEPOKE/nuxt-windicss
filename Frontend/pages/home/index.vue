7f736381b6c7ec5a2f2cffba4a25e7eed4c677a2
Poundpitak Amornlerdlittichai <Damon1fx@gmail.com>
2022-11-01 10:44:31 +0700

    dwl

--- Frontend/pages/home/index.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="bg-gray-500 w-1/3 rounded-lg mt-6">Home</div>
-</template>
-<script>
-export default {};
-</script>
--- Frontend/pages/index.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="bg-gray-500 w-1/3 rounded-lg mt-6">index</div>
-</template>
-<script>
-export default {
-  name: "IndexVue",
-};
-</script>
