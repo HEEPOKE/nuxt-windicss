1b64479c64ab94eadd6ef781cb01fc35acca72fb
Poundpitak Amornlerdlittichai <Damon1fx@gmail.com>
2022-11-01 12:08:10 +0700

    set

---
+++ Frontend/nuxt.config.ts
@@ -0,0 +1,21 @@
+// import { defineNuxtConfig } from "nuxt/config";
+
+export default defineNuxtConfig({
+  // app: {
+  //   head: {
+  //     title: process.env.NameApp,
+  //     meta: [
+  //       { charset: "utf-8" },
+  //       { name: "viewport", content: "width=device-width, initial-scale=1.0" },
+  //       {
+  //         hid: "description",
+  //         name: "description",
+  //         content: process.env.DescriptionApp,
+  //       },
+  //     ],
+  //     link: [],
+  //     script: [],
+  //   },
+  // },
+  modules: ["nuxt-windicss"],
+});
---
+++ Frontend/pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="bg-gray-800">index</div>
+</template>
+<script>
+export default {
+  name: "IndexVue",
+};
+</script>
